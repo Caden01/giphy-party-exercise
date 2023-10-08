916c41b37fad4ec70430003c8c2813cc0e2eb633
Caden01 <caden.carlson01@gmail.com>
2023-10-08 07:07:48 -0700

    adding javascript that allows user to put in a search term that adds a random matching gif to the page

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("Let's get this party started!");
+let input = $("#search");
+let gifs = $("#gifs");
+
+$("form").on("submit", async function (e) {
+  e.preventDefault();
+
+  let search = input.val();
+  input.val("");
+  console.log(search);
+
+  let res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: { q: search, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
+  });
+  createGif(res.data);
+});
+
+function createGif(res) {
+  console.log(res);
+  let results = res.data.length;
+  let index = Math.floor(Math.random() * results);
+  let col = $("<div>", { class: " col-md-4 col-12 mb-4" });
+  let newGif = $("<img>", {
+    src: res.data[index].images.original.url,
+    class: "w-100",
+  });
+  col.append(newGif);
+  gifs.append(col);
+}
+
+$("#remove-btn").on("click", function () {
+  gifs.empty();
+});
